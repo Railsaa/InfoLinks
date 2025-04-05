98bf21bda80859697c76b0831e119f4f374456c9
Railsa <railsaandrade2email@gmail.com>
2025-04-05 15:22:23 -0300

    inserindo novos elementos

--- src/app/inde.tsx
+++ src/app/inde.tsx
@@ -1,9 +1,15 @@
 import { View, Text } from "react-native"
 
+//Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index () {
     return (
+        //Componente Text sendo usando para Renderizar um texto na tela
         <View>
             <Text>Eu sou o millior!!!</Text>
+
+            <Text>Railsa</Text>
+            
+            <Text>Técnico em Informática</Text>
         </View>
     );
 }
